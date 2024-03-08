00fb1bffd1fce14330940558e5c0fbf1275fca63
Ayoub Chaaoui <chaaoui.ayoub@youcan.shop>
2024-03-08 16:24:52 +0100

    add some back-end for listing products on getting single product detail (updating product WIP).

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
+const response = await useFetch('/api/products/4dc88659-3e52-446c-9d3e-668f40de4d50');
+
+try {
+    const res = await $fetch("/api/products/4dc88659-3e52-446c-9d3e-668f40de4d50", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({ name: "AYOUB" }),
+  });
+
+  console.log("======", res);
+
+} catch (e) {
+  console.log("error", e);
+}
+
+// const response  = await res.json();
+
+// fetch("/api/products/").then(res => res.json).then()
 
 </script>
 
 <template>
-  <div>
-    Hello, I'm the app.
-  </div>
+  <pre>
+  {{ JSON.stringify(response.data.value, null, 2) }}
+  </pre>
 </template>
 
 <style>
