689965d09ce4a5e4deeaabd7ef1d26f2039edddb
Eihab Khan <143792300+eihabkhan1@users.noreply.github.com>
2024-03-08 15:24:00 +0100

    add product list and skeleton

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Product } from '#components'
+import { Product, ProductsSkeleton } from '#components'
 </script>
 
 <template>
@@ -7,9 +7,10 @@ import { Product } from '#components'
     <div class="flex flex-col gap-4">
       <h1 class="text-4xl font-bold">Your products</h1>
       <div
-        class="border border-gray-200 p-4 bg-[#F7F7F7] rounded-lg h-[calc(100vh-220px)] overflow-y-scroll"
+        class="border border-gray-200 rounded-lg h-[calc(100vh-220px)] overflow-y-scroll"
       >
-        <ul class="flex flex-col gap-4">
+        <!-- <ProductsSkeleton /> -->
+        <ul class="flex flex-col gap-4 p-4 bg-[#F7F7F7]">
           <Product />
           <Product />
           <Product />
